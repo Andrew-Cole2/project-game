3342ba8e8d257d6ea9f5531e537d4ecc76c20701
Andrew Cole <awc2@live.missouristate.edu>
2022-11-01 20:15:28 -0500

    combine work

--- Clicker Game/Assets/openMenu.cs
+++ Clicker Game/Assets/openMenu.cs
@@ -6,6 +6,11 @@ public class openMenu : MonoBehaviour
 {
     public GameObject Panel;
 
+    private void Start()
+    {
+        Panel.SetActive(false);
+    }
+
     public void OpenPanel() {
         if (Panel != null) {
             bool isOpen = Panel.activeSelf;
